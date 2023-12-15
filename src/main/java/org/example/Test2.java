7478b0a2cdda22b6d880516e42d1c8f6e1d1c58a
cmckibben <151051216+cmckibben@users.noreply.github.com>
2023-12-15 15:03:57 +0000

    Added test 2

---
+++ src/main/java/org/example/Test2.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Test2 {
+    public static void main(String[] args) {
+        String[] names = {"Jamal", "Emily", "Destiny", "Mateo", "Sofia"};
+        for(int i = 0; i < names.length; i++)
+            System.out.println(names[i]);
+    }
+}
